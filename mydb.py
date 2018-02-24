35ad11d5d787a154f6edb256e094d749f15bcb2b
zhangjun2 <512269813@qq.com>
2018-02-24 16:11:50 +0800

    python基础练习

--- mydb.py
+++ mydb.py
@@ -1,54 +1,54 @@
-# # -*- coding:utf-8 -*-
-# _author__ = 'ZHANGJUN'
-# import mysql.connector
-#
-# # 连接配置第一种方式，连接配置，Ip 端口，用户名，密码，数据库名称，字符编码
-# mysql_config = {
-# 	"host": '172.18.50.11',
-# 	'port': 3306,
-# 	'user': 'wanjiaweb',
-# 	'passwd': 'web@123456',
-# 	'db': 'wanjiaweb',
-# 	'charset': 'utf8'
-#
-# }
-# # 第二种连接配置方式
-# # con = mysql.connector.connect(
-# # 	host='172.18.50.11',
-# # 	port=3306,
-# # 	user='wanjiaweb',
-# # 	passwd='web@123456',
-# # 	db='wanjiaweb'
-# # )
-# con = mysql.connector.connect(**mysql_config)
-# cur = con.cursor()
-#
-#
-# # cur.execute('select * from t_address_info')
-#
-# # cur.execute('select * from %s  WHERE DISTRICT_CODE= %s' % ('t_address_info', '2080'))
-# # cur.execute('select * from %s  WHERE PROVINCE_CODE= %s' % ('t_address_info', '2080'))
-# # result = cur.fetchall()  # 查询全部
-# # result = cur.fetchmany(10)  # 查询部分
-# # result = cur.fetchone()  # 查询一条 ,这时result为一个tuple，
-# # print(len(result))
-# # for x in result:
-# # 	print(x)
-# # con.commit()
-# # cur.close()
-#
-#
-# def execute_query(query, size=1):
-# 	query_set = []
-# 	if isinstance(query, str):
-# 		cur.execute(query)
-# 		if size == 1:
-# 			query_set = cur.fetchone()
-# 		else:
-# 			query_set = cur.fetchmany(10)
-# 	return query_set
-#
-#
-# if __name__ == '__main__':
-# 	result = execute_query('select * from t_address_info')
-# 	print(result)
+# -*- coding:utf-8 -*-
+_author__ = 'ZHANGJUN'
+import mysql.connector
+
+# 连接配置第一种方式，连接配置，Ip 端口，用户名，密码，数据库名称，字符编码
+mysql_config = {
+	"host": '172.18.50.11',
+	'port': 3306,
+	'user': 'wanjiaweb',
+	'passwd': 'web@123456',
+	'db': 'wanjiaweb',
+	'charset': 'utf8'
+
+}
+# 第二种连接配置方式
+# con = mysql.connector.connect(
+# 	host='172.18.50.11',
+# 	port=3306,
+# 	user='wanjiaweb',
+# 	passwd='web@123456',
+# 	db='wanjiaweb'
+# )
+con = mysql.connector.connect(**mysql_config)
+cur = con.cursor()
+
+
+# cur.execute('select * from t_address_info')
+
+# cur.execute('select * from %s  WHERE DISTRICT_CODE= %s' % ('t_address_info', '2080'))
+# cur.execute('select * from %s  WHERE PROVINCE_CODE= %s' % ('t_address_info', '2080'))
+# result = cur.fetchall()  # 查询全部
+# result = cur.fetchmany(10)  # 查询部分
+# result = cur.fetchone()  # 查询一条 ,这时result为一个tuple，
+# print(len(result))
+# for x in result:
+# 	print(x)
+# con.commit()
+# cur.close()
+
+
+def execute_query(query, size=1):
+	query_set = []
+	if isinstance(query, str):
+		cur.execute(query)
+		if size == 1:
+			query_set = cur.fetchone()
+		else:
+			query_set = cur.fetchmany(10)
+	return query_set
+
+
+if __name__ == '__main__':
+	result = execute_query('select * from t_address_info')
+	print(result)
--- numpytest.py
+++ numpytest.py
@@ -3,7 +3,35 @@ _author__ = 'ZHANGJUN'
 
 import numpy as np
 
-a = [1, 2, 3, 4]
-b = np.array(a)
+# a = [1, 2, 3, 4]
+# b = np.array(a)
+#
+# print(b.shape)
+# c =np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+# print(c)
+# print(c[0][0][0])
 
-print(b.shape)
+from datetime import date
+
+from datetime import datetime
+# from datetime import time
+import time
+
+# now = datetime.now()  # 日期时间
+
+now = date.today()  # 日期
+
+nowdate = date.fromtimestamp(time.time())
+
+print(now)
+print(now.toordinal())
+print(nowdate)
+print(time.localtime())
+
+print(date.fromordinal(736713))
+# 输出结果：2018-01-19
+
+birthday = date(1992, 1, 17)
+age = nowdate - birthday
+print(age.days)
+
